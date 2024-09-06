36e11cfca08efab387ffc26e5e79c6701fbcf2b7
Reyes Rondon <reyes192119@gmail.com>
2024-09-06 12:23:59 -0400

    home content added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import {Cards} from "./components/cards/Cards"
+import {Heading} from "./components/home/Heading"
+
 export default function Home() {
   return (
     <>
       <h1 className="hidden">Dev Freebies</h1>
+      <Heading />
+      <p className="flex justify-center items-center text-xl">
+        Discover a collection of valuable websites and resources to streamline
+        and enhance your projects
+      </p>
       <Cards />
     </>
   )
